889919ce52718fa3657f49c5328100262ab4e5f5
keith <kdang@quartermaster.house>
2019-02-03 18:47:00 -0500

    fizzbuzz init

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>){
     println("Hello World")
+    fizzbuzz(20)
+}
+
+fun fizzbuzz(num:Int){
+    for(i in 0 until num){
+        when{
+            i % 3 == 0 && i % 5 == 0 -> println("fizzbuzz")
+            i % 3 == 0 -> println("fizz")
+            i % 5 == 0 -> println("buzz")
+            else -> println(i)
+        }
+    }
 }
